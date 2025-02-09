dd98eb965b1139f389f2b2e2ddfe67ae3ccd4cdf
wanglusheng <wanglusheng@axera-tech.com>
2025-02-09 14:45:18 +0800

    bump version to 0.1.1

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ from setuptools import setup
 
 setup(
     name="axengine",
-    version="0.1.0",
+    version="0.1.1",
     classifiers=[
         "Development Status :: 1 - Alpha",
         "License :: OSI Approved :: BSD License",
