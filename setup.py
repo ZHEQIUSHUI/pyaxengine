1312a61d9d79679d82ec3a46f4a6db11d2cf8968
BUG1989 <248857878@qq.com>
2025-04-24 11:09:03 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ from setuptools import setup
 
 setup(
     name="axengine",
-    version="0.1.2",
+    version="0.1.3",
     classifiers=[
         "Development Status :: 1 - Alpha",
         "License :: OSI Approved :: BSD License",
